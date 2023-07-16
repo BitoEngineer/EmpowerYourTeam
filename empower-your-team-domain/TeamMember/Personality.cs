6b3a5ee50e46b6f99f0e8752bf84aff80dc5b818
BitoEngineer <ngp.cardoso@gmail.com>
2023-07-16 17:37:28 +0100

    Create TeamMember and Personality, as well as each personality trait description

---
+++ empower-your-team-domain/TeamMember/Personality.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empower_your_team_domain.TeamMember
+{
+    public class Personality
+    {
+        public int Aggreableness { get; set; }
+        public int Compassion { get; set; }
+        public int Politeness { get; set; }
+        public int Conscientiouness { get; set; }
+        public int Industriousness { get; set; }
+        public int Orderliness { get; set; }
+        public int Extraversion { get; set; }
+        public int Enthusiasm { get; set; }
+        public int Assirteveness { get; set; }
+        public int Neuroticism { get; set; }
+        public int Withdrawal { get; set; }
+        public int Volatily { get; set; }
+        public int OpenenssToExperience { get; set; }
+        public int Intellect { get; set; }
+        public int Openess { get; set; }
+    }
+}
